39514c9e96df83dbb006f8683b87c283a30ef8c4
Arif Fadillah <fadillah.arif34543@gmail.com>
2026-06-01 18:30:48 +0700

    add TLS support for Upstash Redis

--- api/database/database.go
+++ api/database/database.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"context"
+	"crypto/tls"
 	"os"
 
 	"github.com/go-redis/redis/v8"
@@ -11,9 +12,10 @@ var Ctx = context.Background()
 
 func CreateClient(dbNo int) *redis.Client {
 	rdb := redis.NewClient(&redis.Options{
-		Addr:     os.Getenv("DB_ADDR"),
-		Password: os.Getenv("DB_PASS"),
-		DB:       dbNo,
+		Addr:      os.Getenv("DB_ADDR"),
+		Password:  os.Getenv("DB_PASS"),
+		DB:        dbNo,
+		TLSConfig: &tls.Config{},
 	})
 	return rdb
 }
